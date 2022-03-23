113d592f68eddfed845b5bf7b3c0388d96d51635
Audiophonics <webmaster@audiophonics.fr>
2022-03-23 16:06:11 +0100

    Add files via upload

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,24 @@
 #!bin/sh
 starting_dir=`pwd`
-target_distro="$1"
+target_distribution="$1"
+if [ "$target_distribution" = "" ]
+then 
+    read -p "Select the target distribution : moode, volumio, picore " target_distribution
+    case $target_distribution in
+        moode|volumio|picore) 
+			echo $target_distribution 
+			break	;;
+       * ) 
+		   echo $target_distribution is not a valid choice 
+		   exit 1 ;;
+    esac
+fi
+
 out_dir="$starting_dir"/release
 mkdir -p $out_dir/archives
 for file in *
 do 
-    test -f $file/build.sh && cd $file && sh build.sh "$target_distro" && mv release/* $out_dir/archives
+    test -f $file/build.sh && cd $file && sh build.sh "$target_distribution" && mv release/* $out_dir/archives
     cd "$starting_dir"
 done
 
@@ -16,11 +29,11 @@ do
 	a=`tar -xvf $file | grep .sh`
 	sh $a
 done
-' > $out_dir/install_rdm_"$target_distro".sh
+' > $out_dir/install_rdm_"$target_distribution".sh
 
 
 cd $out_dir
-tar -cvzhf install_rdm_"$target_distro".tar.gz archives/* install_rdm_"$target_distro".sh
+tar -cvzhf install_rdm_"$target_distribution".tar.gz archives/* install_rdm_"$target_distribution".sh
 
 
 
